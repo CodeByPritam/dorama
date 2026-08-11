876d781e5732cadbbe9764918b0d10a5050c6145
CodeByPritam <hello@pritamadak.com>
2026-08-11 19:33:40 +0530

    fixed method of /api/v1/auth/:action route

--- src/module/auth/auth.route.ts
+++ src/module/auth/auth.route.ts
@@ -3,7 +3,7 @@ import AuthController from './auth.controller.js';
 
 // Create :: A router instance
 const router = new Hono();
-const AuthRouter = router.get('/auth/:action', AuthController);
+const AuthRouter = router.post('/auth/:action', AuthController);
 
 // Export
 export default AuthRouter;
